a64ad008c995b6aa0ba391b2b5fd3e273ac7a8bf
Islay Anderson <islayanderson@outlook.com>
2024-11-10 00:40:15 +0000

    setting page prototype

---
+++ partials/user_table.php
@@ -0,0 +1,58 @@
+<?php ?>
+
+<button>Kill All Sessions</button>
+<br>
+<table>
+    <tbody>
+        <tr>
+            <th>id</th>
+            <th>username</th>
+            <th>email</th>
+            <th>password</th>
+            <th>first_name</th>
+            <th>last_name</th>
+            <th>permission</th>
+            <th>session</th>
+        </tr>
+        <tr>
+            <td>0</td>
+            <td>root</td>
+            <td>webmaster@ootd.islayanderson.co.uk</td>
+            <td><button>Reset Password</button></td>
+            <td>&nbsp;</td>
+            <td>&nbsp;</td>
+            <td>Admin</td>
+            <td><button>Reset Session</button></td>
+        </tr>
+        <tr>
+            <td>1</td>
+            <td>islay</td>
+            <td>me@islayanderson.co.uk</td>
+            <td><button>Reset Password</button></td>
+            <td>Islay</td>
+            <td>Anderson</td>
+            <td>Admin</td>
+            <td><button>Reset Session</button></td>
+        </tr>
+        <tr>
+            <td>3</td>
+            <td>fakeDave</td>
+            <td>fakeDave@gmail.com</td>
+            <td><button>Reset Password</button></td>
+            <td>Dave</td>
+            <td>Faker</td>
+            <td>User</td>
+            <td><button>Reset Session</button></td>
+        </tr>
+        <tr>
+            <td>4</td>
+            <td>fakeJane</td>
+            <td>fakeJane@gmail.com</td>
+            <td><button>Reset Password</button></td>
+            <td>Jane</td>
+            <td>Faker</td>
+            <td>User</td>
+            <td><button>Reset Session</button></td>
+        </tr>
+    </tbody>
+</table>
